d7783b96454f3528880ca6fc4bba376bf3eea325
Student 8 <Student 8@10.8.33.103>
2017-05-04 12:58:17 -0400

    edited journal entry, set up start screen and transition

--- SymposiumProject/src/survivalAdventureGame/GameScreen.java
+++ SymposiumProject/src/survivalAdventureGame/GameScreen.java
@@ -1,11 +1,19 @@
 package survivalAdventureGame;
 
+import java.awt.Color;
 import java.util.ArrayList;
 
+import gui.components.Action;
+import gui.components.Button;
+import gui.components.TextLabel;
 import gui.components.Visible;
 import gui.screens.ClickableScreen;
 
 public class GameScreen extends ClickableScreen implements Runnable{
+	
+	private TextLabel title;
+	private TextLabel author;
+	private Button gameStarter;
 
 	public GameScreen(int width, int height) {
 		super(width, height);
@@ -16,7 +24,19 @@ public class GameScreen extends ClickableScreen implements Runnable{
 	}
 
 	public void initAllObjects(ArrayList<Visible> viewObjects) {
-		
+		title = new TextLabel(300, 100, 200, 50, "Survival Adventure");
+		author = new TextLabel(300, 150, 200, 50, "By Matthew Yarmolinsky");
+		gameStarter = new Button(300, 200, 200, 50, "Start Game", Color.GREEN, new Action(){
+			public void act() {
+				viewObjects.remove(title);
+				viewObjects.remove(author);
+				viewObjects.remove(gameStarter);
+				//add starter text/tutorial (whatever comes after the initial screen)
+			}
+		});
+		viewObjects.add(title);
+		viewObjects.add(author);
+		viewObjects.add(gameStarter);
 	}
 
 }

81a7300f56c626a4a3669d8ac0c31be61c51b7ca
Student 8 <Student 8@10.8.33.103>
2017-05-05 12:30:07 -0400

    added to journal and edited start transition

--- SymposiumProject/src/survivalAdventureGame/GameScreen.java
+++ SymposiumProject/src/survivalAdventureGame/GameScreen.java
@@ -29,14 +29,13 @@ public class GameScreen extends ClickableScreen implements Runnable {
 		title = new TextLabel(300, 100, 200, 50, "Survival Adventure");
 		author = new TextLabel(300, 150, 200, 50, "By Matthew Yarmolinsky");
 		intro = new TextLabel(300, 100, 700, 300,
-				"Welcome to the Introduction/Tutorial."
-				+ " This is a text- and image-based survival adventure."
-				+ " Your goal is to survive as long as possible."
-				+ " There will be many challenges throughout your time surviving,"
-				+ " but know that they will get harder and harder the longer you progress."
-				+ " Your actions in response to events are only part of what determines your fate,"
-				+ " there is also some random chance involved, so just keep that in mind."
-				+ " Without further ado, you may begin surviving!");
+				"Welcome to the Introduction/Tutorial." + " This is a text- and image-based survival adventure."
+						+ " Your goal is to survive as long as possible."
+						+ " There will be many challenges throughout your time surviving,"
+						+ " but know that they will get harder and harder the longer you progress."
+						+ " Your actions in response to events are only part of what determines your fate,"
+						+ " there is also some random chance involved, so just keep that in mind."
+						+ " Without further ado, you may begin surviving!");
 		gameStarter = new Button(300, 200, 200, 50, "Start Game", Color.GREEN, new Action() {
 			public void act() {
 				viewObjects.remove(title);
@@ -48,13 +47,14 @@ public class GameScreen extends ClickableScreen implements Runnable {
 				exitIntro = new Button(400, 400, 200, 50, "Exit Intro", Color.GREEN, new Action() {
 
 					public void act() {
-						viewObjects.remove(intro);
-						viewObjects.remove(exitIntro);
+						remove(intro);
+						remove(exitIntro);
 					}
-					
+
 				});
-				viewObjects.add(exitIntro);
+				addObject(exitIntro);
 			}
+
 		});
 		viewObjects.add(title);
 		viewObjects.add(author);

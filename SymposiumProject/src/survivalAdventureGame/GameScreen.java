4855735713967b47d1a59d56ff9ea97ddb104d91
Student 8 <Student 8@10.8.33.103>
2017-05-08 13:11:23 -0400

    added to game screen

--- SymposiumProject/src/survivalAdventureGame/GameScreen.java
+++ SymposiumProject/src/survivalAdventureGame/GameScreen.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import gui.components.Action;
 import gui.components.Button;
+import gui.components.TextArea;
 import gui.components.TextLabel;
 import gui.components.Visible;
 import gui.screens.ClickableScreen;
@@ -14,8 +15,9 @@ public class GameScreen extends ClickableScreen implements Runnable {
 	private TextLabel title;
 	private TextLabel author;
 	private Button gameStarter;
-	private TextLabel intro;
+	private TextArea intro;
 	private Button exitIntro;
+	private TextArea text;
 
 	public GameScreen(int width, int height) {
 		super(width, height);
@@ -27,28 +29,31 @@ public class GameScreen extends ClickableScreen implements Runnable {
 
 	public void initAllObjects(ArrayList<Visible> viewObjects) {
 		title = new TextLabel(300, 100, 200, 50, "Survival Adventure");
-		author = new TextLabel(300, 150, 200, 50, "By Matthew Yarmolinsky");
-		intro = new TextLabel(300, 100, 700, 300,
-				"Welcome to the Introduction/Tutorial." + " This is a text- and image-based survival adventure."
-						+ " Your goal is to survive as long as possible."
-						+ " There will be many challenges throughout your time surviving,"
-						+ " but know that they will get harder and harder the longer you progress."
-						+ " Your actions in response to events are only part of what determines your fate,"
-						+ " there is also some random chance involved, so just keep that in mind."
-						+ " Without further ado, you may begin surviving!");
+		author = new TextLabel(300, 150, 250, 50, "By Matthew Yarmolinsky");
+		intro = new TextArea(50, 100, 700, 300,
+				"Welcome to the Introduction/Tutorial." + "This is a text- and image-based survival adventure."
+						+ "Your goal is to survive as long as possible."
+						+ "There will be many challenges throughout your time surviving,"
+						+ "but know that they will get harder and harder the longer you progress."
+						+ "Your actions in response to events are only part of what determines your fate,"
+						+ "there is also some random chance involved, so just keep that in mind."
+						+ "Without further ado, you may begin surviving!");
 		gameStarter = new Button(300, 200, 200, 50, "Start Game", Color.GREEN, new Action() {
 			public void act() {
 				viewObjects.remove(title);
 				viewObjects.remove(author);
 				viewObjects.remove(gameStarter);
 				viewObjects.add(intro);
-				// add starter text/tutorial (whatever comes after the initial
-				// screen)
 				exitIntro = new Button(400, 400, 200, 50, "Exit Intro", Color.GREEN, new Action() {
 
 					public void act() {
 						remove(intro);
 						remove(exitIntro);
+						text = new TextArea(50, 100, 700, 300, "Your story begins here. "
+								+ "You were camping and foolishly forgot to put out the fire before going to sleep and the fire ended up lighting your camp ablaze. "
+								+ "You wake up in the nick of time to grab a few of your things and get out alive. "
+								+ "Choose three items to take with you to aid in your journey to get back to civilization.");
+						addObject(text);
 					}
 
 				});
